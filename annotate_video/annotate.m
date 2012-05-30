31a74cc50e7a8119bacf60188e78c49632126c5a
James Charles <j.charles@leeds.ac.uk>
2012-05-30 15:19:53 +0100

    edited options file to load testing set used in BMVC experiments and reannotated video 22

--- annotate_video/annotate.m
+++ annotate_video/annotate.m
@@ -9,25 +9,32 @@ if exist(data_filename,'file')
     andata = andata.andata;
 else
 %setup data
+    idx_greater = find(size(opts.joints,3)<=opts.testingset,1,'first');
+    if isempty(idx_greater)
+        opts.testingset = opts.testingset;
+    else
+        opts.testingset = opts.testingset(1:idx_greater);
+    end
+    
     joints = opts.joints(:,:,opts.testingset);
     %normalise pose
     joints = bsxfun(@minus,joints,joints(:,1,:));
     joints = reshape(joints,14,length(opts.testingset));
     
     %do clustering
-    andata.centroids = vgg_kmeans(joints,opts.seg.numposeclusters,'verbose',0);
-    andata.joints = zeros(2,7,opts.seg.numposeclusters);
+    andata.centroids = vgg_kmeans(joints,opts.numclusters,'verbose',0);
+    andata.joints = zeros(2,7,opts.numclusters);
     %find poses closest to cluster centroids
-    andata.frameidx = zeros(1,opts.seg.numposeclusters);
+    andata.frameidx = zeros(1,opts.numclusters);
     [c_idx, c_dist] = vgg_nearest_neighbour(joints,andata.centroids);
-    for c = 1:opts.seg.numposeclusters
+    for c = 1:opts.numclusters
         cluster_ids = find(c_idx==c);
         cluster_dist = c_dist(c_idx==c);
         [~,frame_idx] = min(cluster_dist);
         frame_idx = cluster_ids(frame_idx);
         
         while numel(frame_idx)==0
-            r = floor(1+rand*opts.seg.numposeclusters);
+            r = floor(1+rand*opts.numclusters);
             cluster_ids = find(c_idx==r);
             cluster_dist = c_dist(c_idx==r);
             [~,frame_idx] = min(cluster_dist);
@@ -36,7 +43,7 @@ else
         andata.frameidx(c) = opts.testingset(frame_idx);
     end
     andata.frameidx = sort(andata.frameidx);
-    andata.completed = zeros(1,opts.seg.numposeclusters);
+    andata.completed = zeros(1,opts.numclusters);
     andata.data_filename = data_filename;
 %     andata.joints = opts.joints(:,:,andata.frameidx);
     andata.jclr = jet(7);
@@ -45,7 +52,7 @@ end
 %load in images from video
 video_path = sprintf('%s/video%d/%s',opts.video_dir,opts.video_num,opts.video_filename);
 video_obj = open_video_avsf(video_path);
-for c = 1:opts.seg.numposeclusters
+for c = 1:opts.numclusters
     images(:,:,:,c) = read_video_avsf(video_obj,andata.frameidx(c)-1,[opts.stdimgheight,opts.stdimgwidth]);
 end
 close_video_avsf(video_obj);
@@ -80,7 +87,7 @@ andata.pat.toggled = false;
 
 handle.pat.plot_handle = plot_skeleton(zeros(2,7),handle.pat.opts,[]);
 
-handle.title = title(sprintf('frame %d of %d',andata.current,opts.seg.numposeclusters));
+handle.title = title(sprintf('frame %d of %d',andata.current,opts.numclusters));
 handle.btn_next = uicontrol('Style', 'pushbutton', 'String', 'Next',...
         'Position', [70 20 50 20],...
         'Callback',{@button_next,handle,images});
